c80d19f0c1074f83ed862679f6a7b272e309a094
Mereson <chimeremnmaojinta@gmail.com>
2024-08-10 13:05:17 +0100

    sidebar-WIP2

--- src/components/layoutComponents/sidebar/index.jsx
+++ src/components/layoutComponents/sidebar/index.jsx
@@ -1,57 +1,56 @@
-import { NavLink } from "react-router-dom"
 import {
   Datum,
   Logo,
   SbAttendance,
   SbHome,
+  SbLogout,
+  SbNotification,
   SbResults,
   SbSubject,
 } from "../../../assets"
+import { SidebarNavItem } from "../sidebarNav"
 
 export const StudentSidebar = () => {
   return (
-    <aside className="h-[100vh] w-[220px] pl-8 pr-16 bg-[#fafafa]">
-      <i className="pt-8 flex items-center gap-1">
-        <img
-          src={Logo}
-          className="h-[25px] w-[25.97px]"
-          alt="datum logo icon"
-        />
-        <img src={Datum} className="h-[14.14px]" alt="datum logo Title" />
-      </i>
-      <div className="pt-[80px]">
-        <div className="flex flex-col gap-8">
-          {/* 1 */}
-          <NavLink className="flex items-center gap-[10px]">
-            <i className="px-[9px] py-[8px] rounded-lg bg-[#132985]">
-              <SbHome />
-            </i>
-            <p className="font-bold text-[15px] text-[#132985]">Home</p>
-          </NavLink>
-          {/* 2 */}
-          <NavLink className="flex items-center gap-[10px]">
-            <i className="px-[9px] py-[8px] rounded-lg bg-[#efefef]">
-              <SbSubject />
-            </i>
-            <p className="font-bold text-[15px] text-[#8a8a8a]">Subject</p>
-          </NavLink>
-          {/* 3 */}
-          <NavLink className="flex items-center gap-[10px]">
-            <i className="px-[9px] py-[8px] rounded-lg bg-[#efefef]">
-              <SbAttendance />
-            </i>
-            <p className="font-bold text-[15px] text-[#8a8a8a]">Attendance</p>
-          </NavLink>
-          {/* 4 */}
-          <NavLink className="flex items-center gap-[10px]">
-            <i className="px-[9px] py-[8px] rounded-lg bg-[#efefef]">
-              <SbResults />
-            </i>
-            <p className="font-bold text-[15px] text-[#8a8a8a]">Result</p>
-          </NavLink>
+    <aside className="h-[100vh] w-[220px] pl-8 pr-16 pb-10 bg-[#fafafa] flex flex-col justify-between">
+      <div>
+        <i className="pt-8 flex items-center gap-1">
+          <img
+            src={Logo}
+            className="h-[25px] w-[25.97px]"
+            alt="datum logo icon"
+          />
+          <img src={Datum} className="h-[14.14px]" alt="datum logo Title" />
+        </i>
+        <div className="pt-[80px] flex flex-col gap-7">
+          <div className="flex flex-col gap-8">
+            <SidebarNavItem
+              to="/students/dashboard"
+              Icon={SbHome}
+              label={"Home"}
+            />
+            <SidebarNavItem to={"/"} Icon={SbSubject} label={"Subject"} />
+            <SidebarNavItem to={"/"} Icon={SbAttendance} label={"Attendance"} />
+            <SidebarNavItem to={"/"} Icon={SbResults} label={"Result"} />
+            <hr className="" />
+          </div>
+          <div>
+            <SidebarNavItem
+              to={"/"}
+              Icon={SbNotification}
+              label={"Notifications"}
+            />
+          </div>
         </div>
       </div>
-      <div>hey</div>
+      <div className="pt-11">
+        <SidebarNavItem
+          to={"/"}
+          Icon={SbLogout}
+          label="Log out"
+        //   hover={"#f4901f"}
+        />
+      </div>
     </aside>
   )
 }
---
+++ src/components/layoutComponents/sidebarNav/index.jsx
@@ -0,0 +1,38 @@
+import { NavLink } from "react-router-dom"
+import PropTypes from "prop-types"
+
+export const SidebarNavItem = ({ to, Icon, label, hover = "#132a85ec" }) => {
+  return (
+    <NavLink to={to} className={() => `flex items-center gap-[10px] group `}>
+      {({ isActive }) => (
+        <>
+          <i
+            className={`px-[9px] py-[8px] rounded-lg ${
+              isActive
+                ? "bg-[#132985]"
+                : `bg-[#efefef] group-hover:bg-[${hover}] transition-colors duration-200`
+            }`}
+          >
+            <Icon color={isActive ? "#FFFFFF" : "#8A8A8A"} />
+          </i>
+          <p
+            className={`font-bold text-[15px] ${
+              isActive
+                ? "text-[#132985]"
+                : `text-[#8a8a8a] group-hover:text-[${hover}] transition-colors duration-200`
+            }`}
+          >
+            {label}
+          </p>
+        </>
+      )}
+    </NavLink>
+  )
+}
+
+SidebarNavItem.propTypes = {
+  to: PropTypes.string.isRequired,
+  Icon: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  hover: PropTypes.string,
+}
