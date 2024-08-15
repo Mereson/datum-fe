8d0c9c52ea1450d7cd18bc4d56912f8e2394c285
nazzygenpel <nazzyglory@gmail.com>
2024-08-15 13:23:07 +0100

    more progress

--- src/pages/admin/addTeacher/index.jsx
+++ src/pages/admin/addTeacher/index.jsx
@@ -102,7 +102,25 @@ export const AddTeacher = () => {
             id={5}
             name={"Step"}
             query={"Select step"}
-            items={["A+", "A-", "B+", "O+", "O-"]}
+            items={[
+              "01",
+              "02",
+              "03",
+              "04",
+              "05",
+              "06",
+              "07",
+              "08",
+              "09",
+              "10",
+              "11",
+              "12",
+              "13",
+              "14",
+              "15",
+              "16",
+              "17",
+            ]}
           />
         </div>
 
@@ -111,11 +129,121 @@ export const AddTeacher = () => {
             Teaching Details
           </h6>
           <Dropdown
-            id={5}
-            name={"Step"}
+            id={6}
+            name={"Class"}
             query={"Select class(es)"}
-            items={["01", "02", "03", "04", "05", "06"]}
+            items={["JSS1", "JSS2", "JSS3", "SSS1", "SSS2", "SSS3"]}
           />
+
+          <div className={styles.label}>
+            <label htmlFor="Subject"> Subject</label>
+            <input
+              className={styles.inputtext}
+              type="text"
+              placeholder="Subject Name"
+              id="subject"
+              name="subjects"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="grid mt-2 gap-6">
+          <h6 className="mt-2.5  text-[#696969] font-bold grid place-items-center">
+            Contact Information
+          </h6>
+
+          <div className={styles.label}>
+            <label htmlFor="Number"> Phone Number </label>
+            <input
+              className={styles.inputtext}
+              type="text"
+              placeholder="08014196669"
+              id="number"
+              name="number"
+              required
+            />
+          </div>
+
+          <div className={styles.label}>
+            <label htmlFor="Email">Email</label>
+            <input
+              className={styles.inputtext}
+              type="text"
+              placeholder="example@gmail.com"
+              id="email"
+              name="email"
+              required
+            />
+          </div>
+
+          <Dropdown
+            id={7}
+            name={"State of Origin"}
+            query={"Select state of origin"}
+            items={[
+              "Abia",
+              "Abuja",
+              "Adamawa",
+              "Akwa Ibom",
+              "Anambra",
+              "Bauchi",
+              "Bayelsa",
+              "Benue",
+              "Borno",
+              "Cross River",
+              "Delta",
+              "Ebonyi",
+              "Edo",
+              "Ekiti",
+              "Enugu",
+              "Gombe",
+              "Imo",
+              "Jigawa",
+              "Kaduna",
+              "Kano",
+              "Kastina",
+              "Kebbi",
+              "Kogi",
+              "Kwara",
+              "Lagos",
+              "Nassarawa",
+              "Niger",
+              "Ogun",
+              "Ondo",
+              "Osun",
+              "Oyo",
+              "Plateau",
+              "Rivers",
+              "Sokoto",
+              "Taraba",
+              "Yobe",
+              "Zamfara",
+            ]}
+          />
+
+          <div className={styles.label}>
+            <label htmlFor="Local govt"> Local Government of Origin </label>
+            <input
+              className={styles.inputtext}
+              type="text"
+              placeholder="Select local government of origin"
+              id="state"
+              name="state"
+              required
+            />
+          </div>
+
+          <div className={styles.label}>
+            <label htmlFor="Location"> Address </label>
+            <input
+              className={styles.inputtext}
+              type="text"
+              id="Address"
+              name="Address"
+              required
+            />
+          </div>
         </div>
       </div>
 
