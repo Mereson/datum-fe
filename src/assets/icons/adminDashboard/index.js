02ebb4808f2aca868e60abfacd1053c0c7b7987c
mereson <chimeremnmaojinta@gmail.com>
2024-08-22 01:28:59 +0100

    updated admin sidebar icons

--- src/assets/icons/adminDashboard/index.js
+++ src/assets/icons/adminDashboard/index.js
@@ -2,5 +2,6 @@ import TotalParents from "./totalParents"
 import TotalStudents from "./totalStudents"
 import TotalTeachers from "./totalTeachers"
 import NotificationSvg from "./notification.svg"
+import { DashboardIcon } from "./dashboardIcon"
 
-export { TotalParents, TotalStudents, TotalTeachers, NotificationSvg }
+export { TotalParents, TotalStudents, TotalTeachers, NotificationSvg,DashboardIcon }
