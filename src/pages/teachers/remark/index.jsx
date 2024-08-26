9d1db473231eaf533e1c0f1709f9a19d28e79293
nazzygenpel <nazzyglory@gmail.com>
2024-08-26 18:33:38 +0100

    last commit

--- src/pages/teachers/remark/index.jsx
+++ src/pages/teachers/remark/index.jsx
@@ -1,14 +1,20 @@
 export const Remark = () => {
   return (
-    <section>
-      <form action="/submit-remark" method="post">
-        <label htmlFor="remarks"> Remarks </label>
+    <section className="flex justify-center border-2 w-full">
+      <form className="flex flex-col " action="/submit-remark" method="post">
+        <label
+          className="text-[#4f4f4f] text-[32px] font-semibold mb-[24px]"
+          htmlFor="remarks"
+        >
+          Remarks
+        </label>
         <textarea
+          className="mb-[24px]"
           id="remarks"
           name="remarks"
           placeholder="Write your remark..."
-          rows="4"
-          cols="50"
+          rows="8"
+          cols="52"
         ></textarea>
       </form>
     </section>
