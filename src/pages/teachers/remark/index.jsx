8a338ddc7308f8216e79cccc84eba08e89b79bc3
nazzygenpel <nazzyglory@gmail.com>
2024-08-27 07:42:19 +0100

    done with the remark page

--- src/pages/teachers/remark/index.jsx
+++ src/pages/teachers/remark/index.jsx
@@ -1,3 +1,5 @@
+import { FormButton } from "../../../components"
+
 export const Remark = () => {
   return (
     <section className="flex justify-center border-2 w-full">
@@ -9,13 +11,21 @@ export const Remark = () => {
           Remarks
         </label>
         <textarea
-          className="mb-[24px]"
+          className="mb-[24px] pl-4"
           id="remarks"
           name="remarks"
           placeholder="Write your remark..."
           rows="8"
-          cols="52"
+          cols="59"
         ></textarea>
+
+        <FormButton
+          type="submit"
+          content="Save"
+          className={
+            "bg-[#132985] w-[30%] py-[8px] mt-8 text-center rounded-[8px] font-bold text-white cursor-pointer"
+          }
+        />
       </form>
     </section>
   )
